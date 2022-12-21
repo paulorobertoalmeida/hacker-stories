2690385929ca7dc2880f58c5fb008b7375aaf0b2
Paulo Almeida <paulo.roberto.a@hotmail.com>
2022-12-21 14:41:17 -0300

    first commit chap-1

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+const list = [
+  {
+    title: 'React',
+    url: 'http://reactjs.org',
+    author: 'Jordan Walke',
+    points: 4,
+    objectId: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'http://redux.js.org',
+    author: 'Dan Abromov',
+    points: 5,
+    objectId: 1,
+  },
+  {
+    title: 'Harry Potter',
+    url: 'http://harrypotter.com',
+    author: 'JK rolling',
+    points: 7,
+    objectId: 2,
+  }
+]
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Moto Mami</h1>
+      {
+        list.map((item) => {
+          return <div>
+            <ul>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+              <li key={item.objectId}>{item.author}</li>
+              <li key={item.objectId}>{item.points}</li>
+              <li key={item.objectId}>{item.author}</li>
+            </ul>
+          </div>
+        })
+      }
+    </div>
+  );
+}
+
+export default App;
