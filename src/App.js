e95c0af76bde5d2640718cfb95e021f6a083fcd2
Paulo Almeida <paulo.roberto.a@hotmail.com>
2022-12-22 12:48:56 -0300

    event handler- useState

--- src/App.js
+++ src/App.js
@@ -1,28 +1,6 @@
 import React from "react";
 
-const list = [
-  {
-    title: 'React',
-    url: 'http://reactjs.org',
-    author: 'Jordan Walke',
-    points: 4,
-    objectId: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'http://redux.js.org',
-    author: 'Dan Abromov',
-    points: 5,
-    objectId: 1,
-  },
-  {
-    title: 'Harry Potter',
-    url: 'http://harrypotter.com',
-    author: 'JK rolling',
-    points: 7,
-    objectId: 2,
-  }
-]
+const list = [];
 
 //leaf Component
 const List = props =>
@@ -63,8 +41,10 @@ function App() {
       objectId: 2,
     }
   ]
-  const handlerChange = event => {
-    console.log(event);
+  const [searchTerm, setSearchTerm] = React.useState('');
+  
+  const handlerChange = e => {
+    setSearchTerm(e.target.value);
   }
   return (
     <div className="App">
@@ -72,6 +52,8 @@ function App() {
       <hr />
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" onChange={handlerChange} />
+      
+      <p> Searching for <strong>{searchTerm}</strong> </p>
       <List list={stories}/>
     </div>
   );
