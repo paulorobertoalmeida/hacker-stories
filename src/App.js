7e96dbe579c4800334ffa77be63a63562163348b
Paulo Almeida <paulo.roberto.a@hotmail.com>
2022-12-22 12:29:53 -0300

    creating props

--- src/App.js
+++ src/App.js
@@ -25,24 +25,44 @@ const list = [
 ]
 
 //leaf Component
-function List() {
-  return list.map((item) => {
-    return (
-      <div>
-        <ul>
+const List = props =>
+props.list.map((item) => (
+    <div key={item.objectId}>
           <span>
             <a href={item.url}>{item.title}</a>
           </span>
-          <li key={item.objectId}>{item.author}</li>
-          <li key={item.objectId}>{item.points}</li>
-          <li key={item.objectId}>{item.author}</li>
-        </ul>
+          <span key={item.objectId}>{item.author}</span>
+          <span key={item.objectId}>{item.points}</span>
+          <span key={item.objectId}>{item.author}</span>
       </div>
-    );
-  });
-}
+    
+  ));
+
 // root Component
 function App() {
+  const stories = [
+    {
+      title: 'React',
+      url: 'http://reactjs.org',
+      author: 'Jordan Walke',
+      points: 4,
+      objectId: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'http://redux.js.org',
+      author: 'Dan Abromov',
+      points: 5,
+      objectId: 1,
+    },
+    {
+      title: 'Harry Potter',
+      url: 'http://harrypotter.com',
+      author: 'JK rolling',
+      points: 7,
+      objectId: 2,
+    }
+  ]
   const handlerChange = event => {
     console.log(event);
   }
@@ -52,7 +72,7 @@ function App() {
       <hr />
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" onChange={handlerChange} />
-      <List />
+      <List list={stories}/>
     </div>
   );
 }
